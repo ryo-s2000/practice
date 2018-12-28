e03280dd8a3f04187bf9ee67446605bea0af13fd
ryo-s2000 <r-suzuki@fullout.com>
2018-12-28 21:45:47 +0900

    full

---
+++ ruby/cherry/lib/word_synth.rb
@@ -0,0 +1,15 @@
+class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects = effect
+  end
+
+  def play(original_words)
+    @effects.inject(original_words) do |word, effect|
+      effect.call(word)
+    end
+  end
+end
